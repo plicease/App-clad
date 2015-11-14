c34c4df230880ae422859699ea9a60d0e0cf2b3b
Graham Ollis <perl@wdlabs.com>
2015-11-13 20:19:34 -0500

    this will replace the old Clustericious::Admin

---
+++ lib/Clustericious/Admin.pm
@@ -0,0 +1,30 @@
+package Clustericious::Admin;
+
+use strict;
+use warnings;
+
+# ABSTRACT: Parallel SSH client
+# VERSION
+
+=head1 SYNOPSIS
+
+ % perldoc clad
+
+=head1 DESCRIPTION
+
+This module used to contain the machinery to implement the L<clad> command.
+This was moved into L<App::clad> when it was rewritten.  This module is
+provided for compatibility.  In the future it may provide a Perl level API
+for L<clad>
+
+=head1 SEE ALSO
+
+=over 4
+
+=item L<clad>
+
+=back
+
+=cut
+
+1;
