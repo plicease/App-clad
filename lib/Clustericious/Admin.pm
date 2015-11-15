9edb9ef3f7de045ade662a63473d192d09d5a1a2
Graham Ollis <perl@wdlabs.com>
2015-11-15 09:26:24 -0500

    compatability

--- lib/Clustericious/Admin.pm
+++ lib/Clustericious/Admin.pm
@@ -2,6 +2,9 @@ package Clustericious::Admin;
 
 use strict;
 use warnings;
+use App::clad;
+use Carp ();
+use PerlX::Maybe qw( maybe );
 
 # ABSTRACT: Parallel SSH client
 # VERSION
@@ -26,5 +29,49 @@ for L<clad>
 =back
 
 =cut
+
+sub banners
+{
+  (undef) = @_;
+  Carp::carp "Class method call of Clustericious::Admin->banners is deprecated";
+  ();
+}
+
+sub clusters
+{
+  my $self = shift;
+  
+  ref $self
+  ? $self->SUPER::new($_)
+  : do {
+    Carp::carp "Class method call of Clustericious::Admin->clusters is deprecated";
+    sort keys %{ App::clad->new('--server')->cluster_list };
+  };
+}
+
+sub aliases
+{
+  (undef) = @_;
+  Carp::carp "Class method call of Clustericious::Admin->aliases is deprecated";
+  sort keys %{ App::clad->new('--server')->alias };
+}
+
+sub run
+{
+  my $self = shift;
+  
+  ref $self
+  ? $self->SUPER::new(@_)
+  : do {
+    Carp::carp "Class method call of Clustericious::Admin->run is deprecated";
+    my($opts, $cluster, @cmd) = @_;
+    App::clad->new(
+      ($opts->{n} ? ('-n') : ()),
+      ($opts->{l} ? ('-l' => $opts->{l}) : ()),
+      ($opts->{a} ? ('-a') : ()),
+      $cluster, @cmd,
+    )->run;
+  };
+}
 
 1;
